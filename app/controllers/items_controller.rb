60ed5479486df66295e0d8ab1dcdd1fcc0e43097
Isaac <izaroot327@gmail.com>
2021-06-10 18:09:00 -0400

    Completed Lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items
+    else
+      items = Item.all
+      render json: items, include: :user 
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: {"errors": "User not found"}, status: :not_found
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    item = Item.find(params[:id])
+    render json: item
+  rescue ActiveRecord::RecordNotFound
+    render json: {"errors": "Item not found"}, status: :not_found
+  end
+
+  def create
+    # byebug
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
